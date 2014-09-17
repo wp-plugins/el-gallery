8303aa5b1252a223ac968c99c0f9b1e3b3709f76
Eric Lowry <Eric Lowry@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-09-17 09:54:13 +0000

    Version 1.2.6
    git-svn-id: https://plugins.svn.wordpress.org/el-gallery/tags/1.2.6@991744 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ js/el-gallery_admin.js
@@ -0,0 +1,20 @@
+/*
+Plugin Name: EL-Gallery
+Description: An extremely simplistic gallery replacement plugin.
+Version: 1.2.6
+Author: Eric Lowry
+Author URI: http://ericlowry.fr/
+License: GPL2
+*/
+
+( function($){
+
+	$('.el-admin_toggler').each(function(){
+		var current = $(this);
+		$('.el-admin_toggler_box',this).bind("click", function(event) {
+			$('.el-admin_toggle',current).slideToggle();
+			$('i.el-admin_toggler_box',current).toggleClass('fa-caret-right').toggleClass('fa-caret-down');
+		});
+	});
+
+})( jQuery );
